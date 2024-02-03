ced8a69e4b3f81a114247a29ac796f92db327b4e
AshleyJoyE <136359789+AshleyJoyE@users.noreply.github.com>
2024-02-03 14:03:45 -0500

    started routing

---
+++ src/Components/LandingPage.jsx
@@ -0,0 +1,9 @@
+function LandingPage () {
+
+    return (
+        <div>
+            <label>hi</label>
+        </div>
+    );
+};
+export default LandingPage;
---
+++ src/Components/SearchPage.jsx
@@ -0,0 +1,9 @@
+function SearchPage () {
+
+    return (
+        <div>
+            hi
+        </div>
+    );
+};
+export default SearchPage;
